68d098e513bb9be88d86ac68fedbdd843e8025cb
BZzzzi <khj23465@naver.com>
2025-12-26 13:17:36 +0900

    Style: global.css 설정
    - 색상 팔레트 적용
    - 디자인 시안 글꼴 적용
    - 기본 CSS 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spoqaHanSans = localFont({
+  src: [
+    { path: "../../public/font/SpoqaHanSansNeo-Thin.woff2", weight: "100" },
+    { path: "../../public/font/SpoqaHanSansNeo-Light.woff2", weight: "300" },
+    { path: "../../public/font/SpoqaHanSansNeo-Regular.woff2", weight: "400" },
+    { path: "../../public/font/SpoqaHanSansNeo-Medium.woff2", weight: "500" },
+    { path: "../../public/font/SpoqaHanSansNeo-Bold.woff2", weight: "700" },
+  ],
+  variable: "--font-spoqa",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko" className={`${spoqaHanSans.variable}`}>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
