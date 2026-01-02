d4f8c3d516ff6cd6ac88f10ce98bdf65b46984d7
Oh Young gyo <oey157@nate.com>
2026-01-02 16:21:45 +0900

    Feat : 수정

--- src/app/(user)/notice/notices-detail/page.tsx
+++ src/app/(user)/notice/notices-detail/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-
-import Footer from "@/components/common/header/footer";
-import Header from "@/components/common/header/header";
+import Footer from "@/components/common/layouts/footer";
+import Header from "@/components/common/layouts/header";
 import NoticeListWithDetailPage from "@/components/domain/noticesdetail/noticesdetail";
 
 export default function Notices_detail() {
