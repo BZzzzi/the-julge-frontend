e947b4518e9a7285fbc6df9ba12dbf3134a97af6
jangseoyeong <suhyoung030327@naver.com>
2026-01-05 14:53:56 +0900

    Feat: 헤더, 풋터 추가

--- src/app/(owner)/shops/edit/page.tsx
+++ src/app/(owner)/shops/edit/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/common/button";
+import Header from "@/components/common/layouts/header";
 import { apiClient } from "@/lib/api";
 import { getUserIdFromToken } from "@/lib/auth";
 import { cookies } from "next/headers";
@@ -12,54 +13,64 @@ export default async function ShopEditPage() {
 
   if (!token) {
     return (
-      <section className="bg-gray-10 flex min-h-screen items-center justify-center px-4">
-        <div className="border-gray-20 w-full max-w-md rounded-xl border bg-white p-14 text-center">
-          <Image
-            src="/icon/caution.svg"
-            alt="가게 없음"
-            width={48}
-            height={48}
-            className="mx-auto mb-6"
-          />
-          <p className="mb-6 text-lg text-black">로그인이 필요합니다</p>
-          <Link href={"/login"}>
-            <Button
-              variant="primary"
-              size="lg"
-              className="w-86.5"
-            >
-              로그인 하기
-            </Button>
-          </Link>
-        </div>
-      </section>
+      <>
+        <Header />
+        <main>
+          <section className="bg-gray-10 flex min-h-screen items-center justify-center px-4">
+            <div className="border-gray-20 w-full max-w-md rounded-xl border bg-white p-14 text-center">
+              <Image
+                src="/icon/caution.svg"
+                alt="가게 없음"
+                width={48}
+                height={48}
+                className="mx-auto mb-6"
+              />
+              <p className="mb-6 text-lg text-black">로그인이 필요합니다</p>
+              <Link href={"/login"}>
+                <Button
+                  variant="primary"
+                  size="lg"
+                  className="w-86.5"
+                >
+                  로그인 하기
+                </Button>
+              </Link>
+            </div>
+          </section>
+        </main>
+      </>
     );
   }
 
   const userId = getUserIdFromToken(token);
   if (!userId) {
     return (
-      <section className="bg-gray-10 flex min-h-screen items-center justify-center px-4">
-        <div className="border-gray-20 w-full max-w-md rounded-xl border bg-white p-14 text-center">
-          <Image
-            src="/icon/caution.svg"
-            alt="가게 없음"
-            width={48}
-            height={48}
-            className="mx-auto mb-6"
-          />
-          <p className="mb-6 text-lg text-black">로그인 정보가 올바르지 않습니다.</p>
-          <Link href={"/login"}>
-            <Button
-              variant="primary"
-              size="lg"
-              className="w-86.5"
-            >
-              다시 로그인 하기
-            </Button>
-          </Link>
-        </div>
-      </section>
+      <>
+        <Header />
+        <main>
+          <section className="bg-gray-10 flex min-h-screen items-center justify-center px-4">
+            <div className="border-gray-20 w-full max-w-md rounded-xl border bg-white p-14 text-center">
+              <Image
+                src="/icon/caution.svg"
+                alt="가게 없음"
+                width={48}
+                height={48}
+                className="mx-auto mb-6"
+              />
+              <p className="mb-6 text-lg text-black">로그인 정보가 올바르지 않습니다.</p>
+              <Link href={"/login"}>
+                <Button
+                  variant="primary"
+                  size="lg"
+                  className="w-86.5"
+                >
+                  다시 로그인 하기
+                </Button>
+              </Link>
+            </div>
+          </section>
+        </main>
+      </>
     );
   }
 
@@ -68,29 +79,41 @@ export default async function ShopEditPage() {
 
   if (!shop) {
     return (
-      <section className="bg-gray-10 flex min-h-screen items-center justify-center px-4">
-        <div className="border-gray-20 w-full max-w-md rounded-xl border bg-white p-14 text-center">
-          <Image
-            src="/icon/caution.svg"
-            alt="가게 없음"
-            width={48}
-            height={48}
-            className="mx-auto mb-6"
-          />
-          <p className="mb-6 text-lg text-black">등록된 가게가 없습니다.</p>
-          <Link href={"/shops/new"}>
-            <Button
-              variant="primary"
-              size="lg"
-              className="w-86.5"
-            >
-              가게 등록하기
-            </Button>
-          </Link>
-        </div>
-      </section>
+      <>
+        <Header />
+        <main>
+          <section className="bg-gray-10 flex min-h-screen items-center justify-center px-4">
+            <div className="border-gray-20 w-full max-w-md rounded-xl border bg-white p-14 text-center">
+              <Image
+                src="/icon/caution.svg"
+                alt="가게 없음"
+                width={48}
+                height={48}
+                className="mx-auto mb-6"
+              />
+              <p className="mb-6 text-lg text-black">등록된 가게가 없습니다.</p>
+              <Link href={"/shops/new"}>
+                <Button
+                  variant="primary"
+                  size="lg"
+                  className="w-86.5"
+                >
+                  가게 등록하기
+                </Button>
+              </Link>
+            </div>
+          </section>
+        </main>
+      </>
     );
   }
 
-  return <ShopEditForm initialShop={shop} />;
+  return (
+    <>
+      <Header />
+      <main>
+        <ShopEditForm initialShop={shop} />
+      </main>
+    </>
+  );
 }
