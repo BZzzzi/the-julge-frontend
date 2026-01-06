6b9581bc5bb2df0b277a8ecfc50ec85eb05d6566
BZzzzi <khj23465@naver.com>
2026-01-07 04:25:29 +0900

    Feat: 가게 상세, 수정 및 등록 페이지 추가
    - 가게 상세 조회/수정/신규 등록 페이지 구현
    - 각 페이지에 필요한 도메인 컴포넌트 추가
    - 가게 관리 관련 라우팅 설정

---
+++ src/app/(owner)/notice/notice-detail/[shopId]/new/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/common/layouts/header";
+import NoticeUpsertForm from "@/components/domain/notice-shop/notice-upsert-form";
+
+interface PageProps {
+  params: Promise<{
+    shopId: string;
+  }>;
+}
+export default async function NoticeNewPage({ params }: PageProps) {
+  const { shopId } = await params;
+
+  return (
+    <div className="bg-gray-5 flex min-h-screen flex-col">
+      <Header />
+      <NoticeUpsertForm shopId={shopId} />
+    </div>
+  );
+}
